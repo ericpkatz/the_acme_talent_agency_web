406d82af865eb0dbc378e17ed62f7de5776d7da8
ericpkatz <ericpkatz@gmail.com>
2024-02-05 12:57:25 -0500

    createUser and skills

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_travel_agency_db');
+const uuid = require('uuid');
 
 const createTables = async()=> {
   const SQL = `
@@ -9,7 +10,7 @@ const createTables = async()=> {
     CREATE TABLE users(
       id UUID PRIMARY KEY,
       username VARCHAR(20) NOT NULL UNIQUE,
-      password VARCHAR(2) NOT NULL
+      password VARCHAR(20) NOT NULL
     );
     CREATE TABLE skills(
       id UUID PRIMARY KEY,
@@ -26,7 +27,25 @@ const createTables = async()=> {
 
 };
 
+const createUser = async({ username, password })=> {
+  const SQL = `
+    INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
+  `;
+  const response = await client.query(SQL, [ uuid.v4(), username, password]);
+  return response.rows[0];
+};
+
+const createSkill = async({ name })=> {
+  const SQL = `
+    INSERT INTO skills(id, name) VALUES ($1, $2) RETURNING * 
+  `;
+  const response = await client.query(SQL, [ uuid.v4(), name]);
+  return response.rows[0];
+};
+
 module.exports = {
   client,
-  createTables
+  createTables,
+  createUser,
+  createSkill
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const {
   client,
-  createTables
+  createTables,
+  createUser,
+  createSkill
 } = require('./db');
 
 const init = async()=> {
@@ -9,6 +11,16 @@ const init = async()=> {
   console.log('connected to database');
   await createTables();
   console.log('tables created');
+  const [moe, lucy, larry, ethyl, dancing, singing, plateSpinning, juggling] = await Promise.all([
+    createUser({ username: 'moe', password: 'moe_pw'}),
+    createUser({ username: 'lucy', password: 'lucy_pw'}),
+    createUser({ username: 'larry', password: 'larry_pw'}),
+    createUser({ username: 'ethyl', password: 'ethyl_pw'}),
+    createSkill({ name: 'dancing'}),
+    createSkill({ name: 'singing'}),
+    createSkill({ name: 'plate spinning'}),
+    createSkill({ name: 'juggling'})
+  ]);
 
 }
 init();
