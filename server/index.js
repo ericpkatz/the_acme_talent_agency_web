1faf51ff18a58b903b99ed37e1b7fe9d00c1ab93
ericpkatz <ericpkatz@gmail.com>
2024-02-05 13:24:27 -0500

    added express and some routes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,35 @@ const {
   fetchUserSkills,
   deleteUserSkill
 } = require('./db');
+const express = require('express');
+const app = express();
+
+app.get('/api/skills', async(req, res, next)=> {
+  try {
+    res.send(await fetchSkills());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/users', async(req, res, next)=> {
+  try {
+    res.send(await fetchUsers());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/users/:id/userSkills', async(req, res, next)=> {
+  try {
+    res.send(await fetchUserSkills(req.params.id));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
 
 const init = async()=> {
   console.log('connecting to database');
@@ -33,11 +62,19 @@ const init = async()=> {
   const userSkills = await Promise.all([
     createUserSkill({ user_id: moe.id, skill_id: plateSpinning.id}),
     createUserSkill({ user_id: moe.id, skill_id: dancing.id}),
-    createUserSkill({ user_id: ethyl.id, skill_id: singing.id})
+    createUserSkill({ user_id: ethyl.id, skill_id: singing.id}),
+    createUserSkill({ user_id: ethyl.id, skill_id: juggling.id})
   ]);
   console.log(await fetchUserSkills(moe.id));
   await deleteUserSkill({ user_id: moe.id, id: userSkills[0].id});
   console.log(await fetchUserSkills(moe.id));
+
+  console.log(`curl localhost:3000/api/users/${ethyl.id}/userSkills`);
+  
+  console.log('data seeded');
+
+  const port = process.env.PORT || 3000;
+  app.listen(port, ()=> console.log(`listening on port ${port}`));
 
 }
 init();
